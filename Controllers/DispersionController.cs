c490d771555dfff5fb5dbe218dc0a3ae7e195272
Daniel de jesus <dani-wissars-13@live.com>
2021-03-22 15:42:57 -0600

    Update2 server

--- Controllers/DispersionController.cs
+++ Controllers/DispersionController.cs
@@ -14,25 +14,7 @@ namespace Api_Capa_de_Servicios.Controllers
     public class DispersionController : ControllerBase
     {
 
-        /// <summary>
-        /// Obtiene una lista de Personas 
-        /// </summary>
-        /// <remarks>
-        /// Buscar personas que cumplan con los parametros de entrada 
-        /// </remarks>
-        /// <response code="200">OK. Devuelve el objeto solicitado.</response>
-        /// <response code="501">Error Verificar Informacion de Entrada</response>  
-        [HttpGet("GetTransferenciaBancaria")]
-        public IActionResult Get()
-        {
-            return Ok();
-        }
-
-        [HttpPost("GetRererencia")]
-        public IActionResult Get1()
-        {
-            return Ok();
-        }
+       
 
     }
 }
